76ae3f64e68690f4f0e0e0bee32143ce29e86488
Almost Engineer <bitsecondal@gmail.com>
2019-04-20 22:46:36 -0500

    Cleaned up code, updated logging and exceptions

--- src/main/java/com/thealmostengineer/kdenlivetoyoutube/App.java
+++ src/main/java/com/thealmostengineer/kdenlivetoyoutube/App.java
@@ -14,13 +14,12 @@ import com.google.common.io.Files;
 
 /**
  * Application to render videos with Kdenlive and upload them to YouTube
+ * 
  * @author almostengineer, Kenny Robinson
  * 
  */
 public class App 
 {
-	static int defaultTimeout = 120;
-	
 	static void logMessage(String message) {
 		System.out.println(message);	
 	}
@@ -31,35 +30,25 @@ public class App
 		InputStream inputStream = new FileInputStream(propertiesFileName); // check for the file
 		Properties properties = new Properties(); 
 		properties.load(inputStream); // load the properties
+		inputStream.close(); // close the input file
 		
 		logMessage("Done loading properties");
-		
-		inputStream.close(); // close the input file
 		return properties;
 	}
 	
-	static Process startKdenlive(String kdenlivePath) throws Exception {
-		logMessage("Starting kdenlive");
-		ProcessBuilder processBuilder = new ProcessBuilder(kdenlivePath);
-		processBuilder.inheritIO();
-//		processBuilder.redirectError(Redirect.INHERIT);
-//		processBuilder.redirectOutput(Redirect.INHERIT);
-		
-		Process process = processBuilder.start();
-		
-		return process;
-	}
-
 	static File[] getFilesInDirectory(String pendingDirectory) {
 		logMessage("Getting files in " + pendingDirectory);
+		
 		File file = new File(pendingDirectory);
 		File[] fileList = file.listFiles();
+		
 		logMessage("Done getting files in " + pendingDirectory);
 		return fileList;
 	}
 
 	static void deleteFolder(File folder) {
 		logMessage("Deleting folder " + folder.getAbsolutePath());
+		
 	    File[] files = folder.listFiles();
 	    if(files!=null) { //some JVMs return null for empty dirs
 	        for(File f: files) {
@@ -71,15 +60,10 @@ public class App
 	        }
 	    }
 	    folder.delete();
+	    
 	    logMessage("Done deleting folder " + folder.getAbsolutePath());
 	}
 
-//	static WebDriver setTimeouts(WebDriver wDriver, int timeInSeconds) {
-//		wDriver.manage().timeouts().implicitlyWait(timeInSeconds, TimeUnit.SECONDS);
-//		wDriver.manage().timeouts().pageLoadTimeout(timeInSeconds, TimeUnit.SECONDS);
-//		return wDriver;
-//	}
-	
 	static void createDirectory(String directoryPath) {
 		File outputDirectoryFile = new File(directoryPath);
 		if (outputDirectoryFile.exists() == false) {
@@ -90,15 +74,18 @@ public class App
 	
 	static void unpackageCompressTar(String filePathToGz, String outputDirectory) throws Exception {
 		logMessage("Uncompressing file " + filePathToGz);
+		
 		// run gunzip on the file if it is compressed
 		if (filePathToGz.endsWith(".gz")) {
 			logMessage("Gz file: " + filePathToGz);
+			
 			ProcessBuilder pbGunzip = new ProcessBuilder("/bin/gunzip", filePathToGz);
 			pbGunzip.inheritIO();
 			pbGunzip.redirectError(Redirect.INHERIT);
 			pbGunzip.redirectOutput(Redirect.INHERIT);
 			
 			logMessage("Starting gunzip for " + filePathToGz);
+			
 			Process processGunzip = pbGunzip.start();
 			processGunzip.waitFor(60, TimeUnit.SECONDS);
 		}
@@ -113,12 +100,14 @@ public class App
 		}
 		
 		logMessage("Tar file: " + filePathToTar);
+		
 		ProcessBuilder pbUntar = new ProcessBuilder("/bin/tar", "-xf", filePathToTar, "-C", outputDirectory);
 		pbUntar.inheritIO();
 		pbUntar.redirectError(Redirect.INHERIT);
 		pbUntar.redirectOutput(Redirect.INHERIT);
 		
 		logMessage("Untarring to " + outputDirectory);
+		
 		Process processUntar = pbUntar.start();
 		processUntar.waitFor(60, TimeUnit.SECONDS);
 		
@@ -150,42 +139,35 @@ public class App
 		File gzFile = new File(filePathToTar);
 		File gzArchiveFile = new File(archiveDirectory + gzFile.getName());
 		Files.move(gzFile, gzArchiveFile);
+		
 		logMessage("Done archiving project");
 	}
 	
 	static void cleanupRenderDirectory(String renderDirectory) {
 		logMessage("Cleaning up render directory");
+		
 		File videoOutputDirectory = new File(renderDirectory);
 		deleteFolder(videoOutputDirectory);
+		
 		logMessage("Done cleaning up render directory");
 	} // end function
-	
-	static void setProcessTimeout(String timeoutValue) {
-		Integer setTimeout = Integer.parseInt(timeoutValue);
-		if (setTimeout < 5) {
-			setTimeout = 60;
-		} // end if
-	} // end function
-	
-	static void loginToYouTubeApi() {
-		logMessage("Logging in to API");
-	}
 	
 	static void renderVideo(String meltPath, String kdenliveFileName, String videoOutputFileName) throws Exception {
 		
 		if (meltPath.isEmpty()) {
-			throw new Exception("One or more values were not provided for rendering the video 1");
+			throw new Exception("Melt path is empty");
 		}
 		if (kdenliveFileName.isEmpty()) {
-			throw new Exception("One or more values were not provided for rendering the video 2");
+			throw new Exception("Kdenlive project file name is empty");
 		}
 		if (videoOutputFileName.isEmpty()) {
-			throw new Exception("One or more values were not provided for rendering the video 3");
+			throw new Exception("Video output file name is empty");
 		}
 		
 		ArrayList<String> pbArguments = new ArrayList<String>();
 		pbArguments.add(meltPath);
 		
+		logMessage("Reading kdenlive file for resolution information");
 		File kdenliveFile = new File(kdenliveFileName);
 		BufferedReader bufferedReader = new BufferedReader(new FileReader(kdenliveFile));
 		String line = "";
@@ -193,7 +175,7 @@ public class App
 		while((line = bufferedReader.readLine()) != null) {
 			if (line.contains("kdenlive:docproperties.profile")) {
 				resolution = line.substring(line.indexOf(">")+1, line.indexOf("</prop"));
-			}
+			} // end if
 		} // end while
 		bufferedReader.close();
 		
@@ -232,14 +214,10 @@ public class App
     public static void main( String[] args )
     {
     	int exitCode = 1;
-//    	Process kdenliveProcess = null;
     	Properties appProperty;
     	
     	try {
-//			Properties appProperty = loadProperties("testing.properties");
     		appProperty = loadProperties("testing.properties");
-//			kdenliveProcess = startKdenlive(appProperty.getProperty("kdenlivePath"));
-//			TimeUnit.SECONDS.sleep(15);
 			
 			File[] pendingFiles = getFilesInDirectory(appProperty.getProperty("pendingDirectory"));
 			for (int i = 0; i < pendingFiles.length; i++) {
@@ -257,9 +235,7 @@ public class App
 					File[] renderDirFile = renderDir.listFiles();
 					
 					for (int j = 0; j < renderDirFile.length; j++) {
-//						kdenliveFileName = renderDirFile[j].getAbsolutePath();
 						if (renderDirFile[j].getAbsolutePath().endsWith("kdenlive")) {
-//						if (kdenliveFileName.endsWith("kdenlive")) {
 							kdenliveFileName = renderDirFile[j].getAbsolutePath();
 							videoOutputFileName = appProperty.getProperty("outputDirectory") + kdenliveFileName.substring(kdenliveFileName.lastIndexOf("/")) + ".mp4";
 							logMessage("Kdenlive: " + kdenliveFileName);
@@ -267,7 +243,6 @@ public class App
 							break;
 						} // end if
 					} // end for
-					
 
 					createDirectory(appProperty.getProperty("outputDirectory"));
 					renderVideo(appProperty.getProperty("meltPath"), kdenliveFileName, videoOutputFileName);// run the kdenlive melt command
@@ -283,12 +258,10 @@ public class App
 				logMessage("Done processing " + pendingFiles[i].getAbsolutePath());
 			} // end for
 			
-			// loop to upload videos
-			File[] videoFiles = getFilesInDirectory(appProperty.getProperty("renderDirectory"));
+			File[] videoFiles = getFilesInDirectory(appProperty.getProperty("outputDirectory"));
 			for (int i = 0; i < videoFiles.length; i++) {
-				// login to api on first go
-
-				//				TODO upload video via YouTube API
+				// TODO login to api on first go
+				// TODO upload video via YouTube API
 			} // end for
 			
 			exitCode = 0;
@@ -299,10 +272,6 @@ public class App
 		}
     
     	logMessage("Shutting down");
-//    	if (kdenliveProcess != null) {
-//    		logMessage("killing Kdenlive");
-//    		kdenliveProcess.destroyForcibly();
-//    	}
     	
     	System.exit(exitCode);
     } // end function
