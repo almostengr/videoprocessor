ed74745223af29f4ac06df7c68737f1cf14ed46d
Almost Engineer <bitsecondal@gmail.com>
2022-05-01 21:39:27 -0500

    Added channel properties

--- Almostengr.VideoProcessor.Api/Services/IChannelPropertiesService.cs
+++ Almostengr.VideoProcessor.Api/Services/IChannelPropertiesService.cs
@@ -1,11 +1,9 @@
-using Almostengr.VideoProcessor.Api.Constants;
 using Almostengr.VideoProcessor.Api.DataTransferObjects;
 
 namespace Almostengr.VideoProcessor.Api.Services
 {
     public interface IChannelPropertiesService
     {
-        // ChannelPropertiesDto GetChannelProperties(ChannelName channelName);
         ChannelPropertiesDto GetChannelProperties(string channelName);
     }
 }
