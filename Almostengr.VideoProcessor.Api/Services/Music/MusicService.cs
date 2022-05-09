71ae4b033a803f383a0a631c4f72c322cbffcafc
Almost Engineer <bitsecondal@gmail.com>
2022-05-09 13:47:02 -0500

    #33 added wildcard for music files

--- Almostengr.VideoProcessor.Api/Services/Music/MusicService.cs
+++ Almostengr.VideoProcessor.Api/Services/Music/MusicService.cs
@@ -39,7 +39,7 @@ namespace Almostengr.VideoProcessor.Api.Services
         
         public string PickRandomMusicTrack()
         {
-            var musicMixes = base.GetDirectoryContents(_appSettings.Directories.MusicDirectory, $"{FileExtension.Mp3}")
+            var musicMixes = base.GetDirectoryContents(_appSettings.Directories.MusicDirectory, $"*{FileExtension.Mp3}")
                 .Where(x => x.ToLower().Contains("mix"));
             Random random = new();
             return musicMixes.ElementAt(random.Next(0, musicMixes.Count()));
