c38fbbe13be9165dd93a83f86949db07349480ff
Almost Engineer <bitsecondal@gmail.com>
2022-05-14 07:10:55 -0500

    Moved ProgramPaths; made appsettings readonly

--- Almostengr.VideoProcessor.Api/Configuration/AppSettings.cs
+++ Almostengr.VideoProcessor.Api/Configuration/AppSettings.cs
@@ -5,37 +5,29 @@ namespace Almostengr.VideoProcessor.Api.Configuration
     public class AppSettings
     {
 #if RELEASE
-        public TimeSpan WorkerServiceInterval { get; set; } = TimeSpan.FromHours(2);
+        public TimeSpan WorkerServiceInterval = TimeSpan.FromHours(2);
 #else
-        public TimeSpan WorkerServiceInterval { get; set; } = TimeSpan.FromSeconds(30);
+        public TimeSpan WorkerServiceInterval = TimeSpan.FromSeconds(30);
 #endif
 
-        public Directories Directories { get; set; } = new();
-        public ProgramPaths ProgramPaths { get; set; } = new();
-        public int ThumbnailFrames { get; set; } = 5;
-        public bool DoRenderVideos { get; set; } = false;
-        public double DiskSpaceThreshold { get; set; } = 0.05;
+        public Directories Directories = new();
+        public readonly int ThumbnailFrames = 5;
+        public readonly bool DoRenderVideos = false;
+        public readonly double DiskSpaceThreshold = 0.05;
     }
 
     public class Directories
     {
 #if RELEASE
-        public string TranscriptBaseDirectory = "/home/almostengineer/Videos/";
-        public string DashCamBaseDirectory = "/home/almostengineer/";
-        public string RhtBaseDirectory = "/home/almostengineer/";
-        public string MusicDirectory = "/home/almostengineer/Music/";
+        public readonly string TranscriptBaseDirectory = "/home/almostengineer/Videos/";
+        public readonly string DashCamBaseDirectory = "/home/almostengineer/";
+        public readonly string RhtBaseDirectory = "/home/almostengineer/";
+        public readonly string MusicDirectory = "/home/almostengineer/Music/";
 #else
-        public string TranscriptBaseDirectory = "/home/almostengineer/Downloads/";
-        public string DashCamBaseDirectory = "/mnt/d74511ce-4722-471d-8d27-05013fd521b3/Kenny Ram Dash Cam";
-        public string RhtBaseDirectory = "/home/almostengineer/Downloads/";
-        public string MusicDirectory = "/mnt/d74511ce-4722-471d-8d27-05013fd521b3/ytvideostructure/07music";
+        public readonly string TranscriptBaseDirectory = "/home/almostengineer/Downloads/transcripts";
+        public readonly string DashCamBaseDirectory = "/mnt/d74511ce-4722-471d-8d27-05013fd521b3/Kenny Ram Dash Cam";
+        public readonly string RhtBaseDirectory = "/home/almostengineer/Downloads/rhtvideos/";
+        public readonly string MusicDirectory = "/mnt/d74511ce-4722-471d-8d27-05013fd521b3/ytvideostructure/07music";
 #endif
-    }
-
-    public class ProgramPaths
-    {
-        public const string TarBinary = "/bin/tar";
-        public const string FfmpegBinary = "/usr/bin/ffmpeg";
-        public const string BashShell = "/bin/bash";
     }
 }
---
+++ Almostengr.VideoProcessor.Api/Constants/ProgramPaths.cs
@@ -0,0 +1,9 @@
+namespace Almostengr.VideoProcessor.Api.Constants
+{
+    public class ProgramPaths
+    {
+        public const string TarBinary = "/bin/tar";
+        public const string FfmpegBinary = "/usr/bin/ffmpeg";
+        public const string BashShell = "/bin/bash";
+    }
+}
