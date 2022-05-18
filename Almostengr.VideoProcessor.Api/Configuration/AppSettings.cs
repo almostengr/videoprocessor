e79b9b559b015a2a15fefd5b85013f8f3e6ec5ea
Almost Engineer <bitsecondal@gmail.com>
2022-05-17 22:00:17 -0500

    Corrected config for appsettings

--- Almostengr.VideoProcessor.Api/Configuration/AppSettings.cs
+++ Almostengr.VideoProcessor.Api/Configuration/AppSettings.cs
@@ -11,9 +11,9 @@ namespace Almostengr.VideoProcessor.Api.Configuration
 
     public class Directories
     {
-        public string TranscriptBaseDirectory = "/home/almostengineer/Downloads/transcripts";
-        public string DashCamBaseDirectory = "/mnt/d74511ce-4722-471d-8d27-05013fd521b3/Kenny Ram Dash Cam";
-        public string RhtBaseDirectory = "/home/almostengineer/Downloads/rhtvideos/";
-        public string MusicDirectory = "/mnt/d74511ce-4722-471d-8d27-05013fd521b3/ytvideostructure/07music";
+        public string TranscriptBaseDirectory { get; set; }
+        public string DashCamBaseDirectory { get; set; }
+        public string RhtBaseDirectory { get; set; }
+        public string MusicDirectory { get; set; }
     }
 }
