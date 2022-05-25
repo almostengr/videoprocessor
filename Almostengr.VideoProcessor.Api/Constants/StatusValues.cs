1472f2b7bde5534f15aabe3abfc0fcffec398668
Almost Engineer <bitsecondal@gmail.com>
2022-05-24 21:57:53 -0500

    Converted enum to constants

---
+++ Almostengr.VideoProcessor.Api/Constants/StatusValues.cs
@@ -0,0 +1,12 @@
+namespace Almostengr.VideoProcessor.Api.Constants
+{
+    public class StatusValues
+    {
+        public const string Extracting = "extracting";
+        public const string ConvertingToMp4 = "converting";
+        public const string Rendering = "rendering";
+        public const string Archiving = "archiving";
+        public const string Cleaning = "cleaning";
+        public const string Idle = "idle";
+    }
+}
