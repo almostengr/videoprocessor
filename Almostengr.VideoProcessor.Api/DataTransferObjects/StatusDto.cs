bceb22726343ada0dd049a8191175f102bfe3a50
Almost Engineer <bitsecondal@gmail.com>
2022-05-29 19:35:22 -0500

    Updated DTO

--- Almostengr.VideoProcessor.Api/DataTransferObjects/StatusDto.cs
+++ Almostengr.VideoProcessor.Api/DataTransferObjects/StatusDto.cs
@@ -6,6 +6,12 @@ namespace Almostengr.VideoProcessor.Api.DataTransferObjects
     public class StatusDto
     {
         public StatusKeys Key { get; set; }
+
+        public string KeyDescription
+        {
+            get { return ((StatusKeys)Key).ToString(); }
+        }
+
         public string Value { get; set; }
         public DateTime LastChanged { get; set; }
     }
--- Almostengr.VideoProcessor.Api/Enums/StatusKeys.cs
+++ Almostengr.VideoProcessor.Api/Enums/StatusKeys.cs
@@ -2,9 +2,9 @@ namespace Almostengr.VideoProcessor.Api.Enums
 {
     public enum StatusKeys
     {
+        DashFile = 1,
+        DashStatus,
         RhtFile,
         RhtStatus,
-        DashFile,
-        DashStatus,
     }
 }
