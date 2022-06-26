315434d06eef3ae4307499beec01eeb72bf933d4
Almost Engineer <bitsecondal@gmail.com>
2022-06-26 17:30:29 -0500

    Updated constants for loading configuration

---
+++ Almostengr.VideoProcessor.Core/Constants/AppConstants.cs
@@ -0,0 +1,9 @@
+namespace Almostengr.VideoProcessor.Core.Constants
+{
+    public sealed class AppConstants
+    {
+        public const string DotNetEnvironment = "DOTNET_ENVIRONMENT";
+        public const string AppSettingsProdFile = "appsettings.json";
+        public const string AppSettingsDevlFile = "appsettings.Development.json";
+    }
+}
---
+++ Almostengr.VideoProcessor.Core/Constants/AppEnvironment.cs
@@ -0,0 +1,8 @@
+namespace Almostengr.VideoProcessor.Core.Constants
+{
+    public sealed class AppEnvironment
+    {
+        public const string Devl = "development";
+        public const string Prod = "production";
+    }
+}
