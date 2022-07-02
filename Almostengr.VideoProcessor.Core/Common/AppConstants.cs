c5c512f2fb9f39ce90ffe4b2134aa55454b75f36
Almost Engineer <bitsecondal@gmail.com>
2022-07-02 14:28:59 -0500

    Code cleanup

--- Almostengr.VideoProcessor.Core/Common/AppConstants.cs
+++ Almostengr.VideoProcessor.Core/Common/AppConstants.cs
@@ -2,7 +2,6 @@ namespace Almostengr.VideoProcessor.Core.Common
 {
     public sealed class AppConstants
     {
-        public const string DotNetEnvironment = "DOTNET_ENVIRONMENT";
         public const string AppSettingsProdFile = "appsettings.json";
         public const string AppSettingsDevlFile = "appsettings.Development.json";
     }
