445bf49fa093f416e12535efa630b0bf0382dfc5
Kenny Robinson <bitsecondal@gmail.com>
2022-10-14 07:54:32 -0500

    Create ChristmasLightVIdeo.cs

---
+++ src/Almostengr.VideoProcessor.Domain/Videos/ChristmastLightShowVideo/ChristmasLightVIdeo.cs
@@ -0,0 +1,45 @@
+using Almostengr.VideoProcessor.Domain.Common;
+
+namespace Almostengr.VideoProcessor.Domain.Videos.ChristmasLightShowVideo;
+
+public sealed record ChristmasLightVIdeo : BaseVideo
+{
+    public readonly string xResolution = "1920";
+    public readonly string yResolution = "1080";
+    public readonly string audioBitRate = "196000";
+    public readonly string audioSampleRate = "48000";
+    // public readonly string NO_INTRO_FILE = "nointro.txt";
+    // public readonly string SHOW_INTRO_FILENAME_MP4 = "rhtservicesintro.mp4";
+    // public readonly string ShowIntroFilePath = "/mnt/d74511ce-4722-471d-8d27-05013fd521b3/ytvideostructure/rhtservicesintro.mp4";
+
+    public ChristmasLightVIdeo(string baseDirectory)
+    {
+        BaseDirectory = baseDirectory;
+    }
+
+    public override string ChannelBannerText()
+    {
+        // Random random = new Random();
+        // string[] bannerText = {
+        //             "rhtservices.net",
+        //             "Robinson Handy and Technology Services",
+        //             "rhtservices.net/facebook",
+        //             "rhtservices.net/instagram",
+        //             "rhtservices.net/youtube",
+        //             "@rhtservicesllc"
+        //             };
+
+        // return bannerText.ElementAt(random.Next(0, bannerText.Length));
+        return ChannelBannerTextHandymanTechnology();
+    }
+
+    public override string TextColor()
+    {
+        return FfMpegColors.White;
+    }
+
+    public override string BoxColor()
+    {
+        return FfMpegColors.Maroon;
+    }
+}
