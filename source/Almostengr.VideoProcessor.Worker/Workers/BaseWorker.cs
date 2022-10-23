d963158ef11573f0a3d928cb268102d1eaac3288
Almost Engineer <bitsecondal@gmail.com>
2022-10-23 11:48:30 -0500

    #51 worker service

--- source/Almostengr.VideoProcessor.Worker/Workers/BaseWorker.cs
+++ source/Almostengr.VideoProcessor.Worker/Workers/BaseWorker.cs
@@ -4,13 +4,6 @@ namespace Almostengr.VideoProcessor.Worker.Workers;
 
 internal abstract class BaseWorker : BackgroundService
 {
-    public readonly TimeSpan WaitDelay;
-
-    protected BaseWorker()
-    {
-        WaitDelay = TimeSpan.FromMinutes(1);
-    }
-
     protected BaseWorker(AppSettings appSettings)
     {
     }
