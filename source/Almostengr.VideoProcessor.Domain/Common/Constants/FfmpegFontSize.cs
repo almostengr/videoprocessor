fe26da5b3e334caafc09fbfbbdbfb8f2babc0cf4
Almost Engineer <bitsecondal@gmail.com>
2023-01-03 16:30:45 -0600

    modified variable declaration

--- source/Almostengr.VideoProcessor.Domain/Common/Constants/FfmpegFontSize.cs
+++ source/Almostengr.VideoProcessor.Domain/Common/Constants/FfmpegFontSize.cs
@@ -2,8 +2,8 @@ namespace Almostengr.VideoProcessor.Domain.Common.Constants;
 
 public static class FfmpegFontSize
 {
-    public static string XLarge = "h/21";
-    public static string Large = "h/28";
-    public static string Medium = "h/35";
-    public static string Small = "h/42";
+    public const string XLarge = "h/21";
+    public const string Large = "h/28";
+    public const string Medium = "h/35";
+    public const string Small = "h/42";
 }
