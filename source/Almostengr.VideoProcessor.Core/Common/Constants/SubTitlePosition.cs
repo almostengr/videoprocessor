5e43de0682bac32e0257285de7a6ef7b67b681e7
Almost Engineer <bitsecondal@gmail.com>
2023-01-25 20:56:12 -0600

    added subtitle positions for ASS subtitles

---
+++ source/Almostengr.VideoProcessor.Core/Common/Constants/SubTitlePosition.cs
@@ -0,0 +1,14 @@
+namespace Almostengr.VideoProcessor.Core.Common.Constants;
+
+public enum SubTitlePosition
+{
+    BottomLeft = 1,
+    BottomCenter = 2,
+    BottomRight = 3,
+    MiddleLeft = 4,
+    MiddleCenter = 5,
+    MiddleRight = 6,
+    TopLeft = 7,
+    TopCenter = 8,
+    TopRight = 9
+}
