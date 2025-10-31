b01dacf9a69423124374106c586c2d1709bd208d
almostengr <info@rhtservices.net>
2025-10-30 20:14:52 -0500

    Updated script for auto organize dash cam files

--- organize_dashcam2_files.sh
+++ organize_dashcam2_files.sh
@@ -6,6 +6,11 @@
 
 INCOMING_DIRECTORY="/mnt/d74511ce-4722-471d-8d27-05013fd521b3/videos/incoming"
 
+# todo - use NR files in the future, but for now remove them
+
+echo "Removing NR files"
+rm *NR*mp4
+
 for file in *NF*mp4
 do
     fileDate=$(/usr/bin/ffprobe "${file}" 2>&1 | /usr/bin/grep "Input" | /usr/bin/awk -F '_' '{print $2}' | /usr/bin/head -c 8)
@@ -18,7 +23,3 @@ do
 
     /usr/bin/mv "${file}" "${newDirectory}"
 done
-
-# todo - use NR files in the future, but for now remove them
-echo "Removing NR files"
-rm *NR*mp4
