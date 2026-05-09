083c6eb49cefef2a92cb63c08b4a592767b0e628
almostengr <info@rhtservices.net>
2026-05-09 15:42:35 -0500

    updated dashcam2 script

---
+++ dashcam2.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+source /mnt/d74511ce-4722-471d-8d27-05013fd521b3/repositories/videoprocessor/common.sh
+
+DEBUG=1
+BASE_DIRECTORY="/mnt/d74511ce-4722-471d-8d27-05013fd521b3/testvideo"
+
+ctaDuration=12
+subscribeBoxColor="green"
+subscribeBoxText="HELP THE CHANNEL GROW BY SUBSCRIBING NOW!"
+bgBoxColor="green"
+channelBrandText="Kenny Ram Dash Cam"
+
+INCOMING_DIRECTORY="${BASE_DIRECTORY}/incoming"
+PROCESSED_DIRECTORY="${BASE_DIRECTORY}/processed"
+ARCHIVE_DIRECTORY="${BASE_DIRECTORY}/archive"
+ACTIVE_FILE="${BASE_DIRECTORY}/.active.txt"
+
+flipRearCameraFiles() {
+	rearCameraFiles=$(ls -1 *NR* | wc -l)
+
+	if [ $rearCameraFiles -gt 0 ]; then
+		for rearCameraFile in *NR*mp4
+		do
+			ffmpeg -i "${rearCameraFile}" -vf "vflip" "${rearCameraFlie}.flipped.mp4"
+
+			# find the front file
+
+			# render with rear file overlaid with front file
+
+			# remove rear and front files
+
+			# rename rendered file
+            mv "${rearCameraFile}.flipped.mp4" "${rearCameraFile}"
+		done
+	fi
+}
+
+exitWhenActiveFilePresent
+
+createMissingDirectories
+
+for videoDirectory in $(ls -1 "${INCOMING_DIRECTORY}")
+do
+    changeToIncomingDirectory
+
+    fullVideoDirectory="${INCOMING_DIRECTORY}/${videoDirectory}"
+    
+    cd "${fullVideoDirectory}" || exit
+
+    exitWhenExcludedFilesPresent
+
+    removePreviousRenderFiles
+
+    lowercaseAllFileNames
+
+    createFfmpegInputFile mp4
+
+    selectMixTrack
+
+    debugMessage "Creating output with graphics file"
+
+    # render the video file without graphics included
+    ffmpeg -y -hide_banner -init_hw_device vaapi=foo:/dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format nv12 -f concat -safe 0 -i ffmpeg.input -i "${MIX_AUDIO_TRACK_FILE}" -filter_hw_device foo -vf "format=vaapi|nv12,hwupload" -vcodec h264_vaapi -shortest -map 0:v:0 -map 1:a:0 "outputNoGraphics.mp4"
+
+    commandReturnCode=$?
+    if [ $commandReturnCode -gt 0 ]; then
+        infoMessage "Rendering with CPU"
+        ffmpeg -y -hide_banner -f concat -safe 0 -i ffmpeg.input -i "${MIX_AUDIO_TRACK_FILE}" -shortest -map 0:v:0 -map 1:a:0 "outputNoGraphics.mp4"
+
+        commandReturnCode=$?
+        if [ $commandReturnCode -gt 0 ]; then
+            errorMessage "Unable to render with CPU"
+            mv "${fullVideoDirectory}" "${ERROR_DIRECTORY}"
+        fi
+    fi
+
+    # add graphics to video
+    videoGraphicsFilter="drawtext=textfile:'${channelBrandText}':fontcolor=white@0.6:fontsize=${fontSize}:${UPPERRIGHT}:box=1:boxcolor=${bgBoxColor}@0.4:boxborderw=10"
+
+    if [ "${subscribeBoxText}" != "" ]; then
+        videoGraphicsFilter="${videoGraphicsFilter},drawtext=text='${subscribeBoxText}':fontcolor=white:box=1:boxcolor=${subscribeBoxColor}@1:boxborderw=20:fontsize=${fontSize}:${LOWERLEFT}:enable='if(lt(t,10),0,if(lt(mod(t-10,${brandDelaySeconds}),${ctaDuration}),1,0))'"
+    fi
+
+    if [ "${followPageText}" != "" ]; then
+        videoGraphicsFilter="${videoGraphicsFilter},drawtext=text='${followPageText}':fontcolor=white:box=1:boxcolor=${followBoxColor}@1:boxborderw=20:fontsize=${fontSize}:${LOWER_LEFT1}:enable='if(lt(t,10),0,if(lt(mod(t-10,${brandDelaySeconds}),${ctaDuration}),1,0))'"
+    fi
+
+    debugMessage "Creating output with graphics file"
+
+    ffmpeg -y -hide_banner -init_hw_device vaapi=foo:/dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format nv12 -i outputNoGraphics.mp4 -filter_hw_device foo -vf "${videoGraphicsFilter}, format=vaapi|nv12,hwupload" -vcodec h264_vaapi -shortest -c:a copy outputFinal.mp4
+
+    commandReturnCode=$?
+    if [ $commandReturnCode -gt 0 ]; then
+        infoMessage "Rendering with CPU"
+        ffmpeg -y -hide_banner -i outputNoGraphics.mp4 -vf "${videoGraphicsFilter}" -shortest -c:a copy outputFinal.mp4;
+
+        commandReturnCode=$?
+        if [ $commandReturnCode -gt 0 ]; then
+            errorMessage "Unable to render with CPU"
+            mv "${fullVideoDirectory}" "${ERROR_DIRECTORY}"
+        fi
+    fi
+
+    # move output file
+    mv outputFinal.mp4 "${ARCHIVE_DIRECTORY}/${videoDirectory}.mp4"
+
+    # archive the file video file and move it
+    tarballArchiveFile="${videoDirectory}.tar.xz"
+
+    infoMessage "Archiving video file ${tarballArchiveFile}"
+    tar -cJf "$tarballArchiveFile" outputNoGraphics.mp4
+
+    returnCode=$?
+    if [ ${returnCode} -gt 0 ]; then
+        errorMessage "Unable to archive video file."
+        mv "${fullVideoDirectory}" "${ERROR_DIRECTORY}"
+    fi
+    mv "${tarballArchiveFile}" "${ARCHIVE_DIRECTORY}/${tarballArchiveFile}"
+
+    # move video directory to Processed directory
+    infoMessage "Moving video directory to Processed folder"
+    changeToIncomingDirectory
+    mv "${videoDirectory}" "${PROCESSED_DIRECTORY}"
+done 
+
+removeActiveFile
